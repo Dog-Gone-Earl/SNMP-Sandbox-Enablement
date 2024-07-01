fcb8086a3ee0d1bd789a9e4c6dbc6c71d57ebc6c
Terry Dennison Jr <107069502+Dog-Gone-Earl@users.noreply.github.com>
2024-07-01 15:38:58 -0600

    Update setup.sh

--- snmp_enablement/snmp_v1/setup.sh
+++ snmp_enablement/snmp_v1/setup.sh
@@ -3,7 +3,9 @@
 
 comm_string=<VALUE>
 echo "Provisioning!"
-sudo apt-get update -y; sudo apt-get upgrade -y; sudo apt-get install -y snmpd snmp
+sudo apt-get update -y
+sudo apt-get install -y snmpd snmp
+#sudo apt-get upgrade -y; 
 sudo sed -i '73i rocommunity '$comm_string'' /etc/snmp/snmpd.conf
 sudo sed -i "s/agentaddress  127.0.0.1,[::1]/agentAddress udp:161,udp6:[::1]:161/1" /etc/snmp/snmpd.conf
 sudo service snmpd restart
